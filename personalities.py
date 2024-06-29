356660a688a857f399b89c99e63f878b91538a4c
AileenAugustus <136139961+AileenAugustus@users.noreply.github.com>
2024-06-30 05:20:43 +0800

    Create personalities.py

---
+++ personalities.py
@@ -0,0 +1,14 @@
+personalities = {
+    "DefaultPersonality": {
+        "api_url": "https://openrouter.ai/api/v1/chat/completions",
+        "prompt": "You are chatgpt.",
+        "temperature": 0.6,
+        "model": "openai/gpt-4o"
+    },
+    "PersonalityName": {
+        "api_url": "https://openrouter.ai/api/v1/chat/completions",
+        "prompt": "Write the prompt here.",
+        "temperature": 1,
+        "model": "openai/gpt-4o"
+    },
+}
